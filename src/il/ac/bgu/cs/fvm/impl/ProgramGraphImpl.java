33cf2b576ed1b1e43cf7b4d44fa27a12589ef6d4
miamiHits <yardench@gmail.com>
2017-12-05 14:48:23 +0200

    Circut Tests - NOT PASS and more 4 different tests

--- src/il/ac/bgu/cs/fvm/impl/ProgramGraphImpl.java
+++ src/il/ac/bgu/cs/fvm/impl/ProgramGraphImpl.java
@@ -3,95 +3,86 @@ package il.ac.bgu.cs.fvm.impl;
 import il.ac.bgu.cs.fvm.programgraph.PGTransition;
 import il.ac.bgu.cs.fvm.programgraph.ProgramGraph;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+/**
+ * Created by Nir on 12/14/16.
+ */
 public class ProgramGraphImpl<L, A> implements ProgramGraph<L, A> {
 
-    private String name;
 
-    private Set<L> initialLocations;
-    private Set<L> locations;
-    private Set<PGTransition<L, A>> transitions;
-    private Set<List<String>> initalizations;
+    private Set<List<String>> mInitializations;
+    private Map<String, Object> mInitialEval;
+    private Set<L> mInitialLocations, mLocations;
+    private Set<PGTransition<L, A>> mTransitions;
+    private String mName;
+
 
     public ProgramGraphImpl() {
-        this.initialLocations = new HashSet<L>();
-        this.locations = new HashSet<L>();
-        this.transitions = new HashSet<PGTransition<L, A>>();
-        this.initalizations = new HashSet<List<String>>();
+        mInitializations = new HashSet<>();
+        mInitialEval = new LinkedHashMap<String, Object>();
+        mInitialLocations = new HashSet<L>();
+        mLocations = new HashSet<L>();
+        mTransitions = new HashSet<>();
     }
 
     @Override
     public void addInitalization(List<String> init) {
-        this.initalizations.add(init);
+        mInitializations.add(init);
     }
 
     @Override
     public void addInitialLocation(L location) {
-        if (this.locations.contains(location)) {
-            this.initialLocations.add(location);
-        } else {
-            throw new RuntimeException(); //TODO
-        }
+        mInitialLocations.add(location);
     }
 
     @Override
-    public void addLocation(L location) {
-        this.locations.add(location);
+    public void addLocation(L l) {
+        mLocations.add(l);
     }
 
     @Override
-    public void addTransition(PGTransition<L, A> transition) {
-        this.transitions.add(transition);
+    public void addTransition(PGTransition<L, A> t) {
+        mTransitions.add(t);
     }
 
     @Override
     public Set<List<String>> getInitalizations() {
-        return this.initalizations;
+        return mInitializations;
     }
 
     @Override
     public Set<L> getInitialLocations() {
-        return this.initialLocations;
+        return mInitialLocations;
     }
 
     @Override
     public Set<L> getLocations() {
-        return this.locations;
+        return mLocations;
     }
 
     @Override
     public String getName() {
-        return this.name;
+        return mName;
     }
 
     @Override
     public Set<PGTransition<L, A>> getTransitions() {
-        return this.transitions;
+        return mTransitions;
     }
 
     @Override
-    public void removeLocation(L location) {
-        if (this.locations.contains(location)) {
-            this.locations.remove(location);
-        } else {
-            throw new RuntimeException(); //TODO
-        }
+    public void removeLocation(L l) {
+        mLocations.remove(l);
     }
 
     @Override
-    public void removeTransition(PGTransition<L,A> transition) {
-        if (this.transitions.contains(transition)) {
-            this.transitions.remove(transition);
-        } else {
-            throw new RuntimeException(); //TODO
-        }
+    public void removeTransition(PGTransition<L, A> t) {
+        mTransitions.remove(t);
     }
 
     @Override
     public void setName(String name) {
-        this.name = name;
+        mName = name;
     }
 }
--- src/il/ac/bgu/cs/fvm/impl/TransitionSystemImpl.java
+++ src/il/ac/bgu/cs/fvm/impl/TransitionSystemImpl.java
@@ -4,224 +4,194 @@ import il.ac.bgu.cs.fvm.exceptions.*;
 import il.ac.bgu.cs.fvm.transitionsystem.Transition;
 import il.ac.bgu.cs.fvm.transitionsystem.TransitionSystem;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-import static il.ac.bgu.cs.fvm.exceptions.TransitionSystemPart.*;
+/**
+ * Created by Nir on 11/28/16.
+ */
+public class TransitionSystemImpl<S, A, P> implements TransitionSystem<S, A, P> {
 
+    private String mName;
 
-public class TransitionSystemImpl<STATE ,ACTION ,ATOMIC_PROPOSITION> implements TransitionSystem<STATE ,ACTION ,ATOMIC_PROPOSITION> {
+    private Set<S> mStates, mInitialStates;
 
-	private Set<STATE> initialStates;// I
-	
-	private Set<Transition<STATE,ACTION>> transitions;// -> 
+    private Set<Transition<S, A>> mTransitions;
 
-	private Set<ATOMIC_PROPOSITION> atomicPropositions;// AP
+    private Set<A> mActions;
 
-	private String nameOfComp;
+    private Set<P> mAtomicPropositions;
 
-	private Set<STATE> states;// S
-
-	private Map<STATE, Set<ATOMIC_PROPOSITION>> labeled;// L
-
-	private Set<ACTION> actions;// Act
+    private HashMap<S, Set<P>> mLabels;
 
 
-	public TransitionSystemImpl(TransitionSystemImpl<STATE, ACTION, ATOMIC_PROPOSITION> other) {
+    public TransitionSystemImpl() {
+        mStates = new HashSet<>();
+        mInitialStates = new HashSet<>();
+        mTransitions = new HashSet<>();
+        mActions = new HashSet<>();
+        mAtomicPropositions = new HashSet<>();
+        mLabels = new LinkedHashMap<>();
+    }
 
-		this.nameOfComp = other.nameOfComp;
-		this.initialStates = new HashSet<STATE>(other.initialStates);
-		this.states = new HashSet<STATE>(other.states);
-		this.labeled = new HashMap<STATE, Set<ATOMIC_PROPOSITION>>();
-		for (STATE state : other.labeled.keySet()) {
-			Set<ATOMIC_PROPOSITION> otherAPsForState = other.labeled.get(state);
-			this.labeled.put(state, new HashSet<ATOMIC_PROPOSITION>(otherAPsForState));
-		}
-		this.actions = new HashSet<ACTION>(other.actions);
-		this.transitions = new HashSet<>(other.transitions);
-		this.atomicPropositions = new HashSet<ATOMIC_PROPOSITION>(other.atomicPropositions);
-	}
-	
-	public TransitionSystemImpl() {
+    @Override
+    public String getName() {
+        return mName;
+    }
 
-		this.initialStates = new HashSet<STATE>();
-		this.states = new HashSet<STATE>();
-		this.labeled = new HashMap<STATE, Set<ATOMIC_PROPOSITION>>();
-		this.actions = new HashSet<ACTION>();
-		this.transitions = new HashSet<Transition<STATE,ACTION>>();
-		this.atomicPropositions = new HashSet<ATOMIC_PROPOSITION>();
-	}
+    @Override
+    public void setName(String name) {
+        mName = name;
+    }
 
-	
-	@Override
-	public String getName() {
-		return this.nameOfComp;
-	}
+    @Override
+    public void addAction(A a) {
+        mActions.add(a);
+    }
 
-	@Override
-	public void setName(String name) {
-		this.nameOfComp = name;
-	}
+    @Override
+    public void addInitialState(S s) throws FVMException {
+        if (mStates.contains(s)) {
+            mInitialStates.add(s);
+        } else {
+            throw new InvalidInitialStateException(s);
+        }
+    }
 
-	public void addAction(ACTION action) {
-		this.actions.add(action);
-	}
+    @Override
+    public void addState(S s) {
+        if (!mStates.contains(s)) {
+            mStates.add(s);
+        }
+    }
 
-	@Override
-	public void addInitialState(STATE initialState) throws FVMException {
-		if (!this.states.contains(initialState)) {
-			throw new InvalidInitialStateException(initialState);
-		}
-		this.initialStates.add(initialState);
-	}
+    @Override
+    public void addTransition(Transition<S, A> t) throws FVMException {
+        if (mStates.contains(t.getFrom()) && mStates.contains(t.getTo()) && mActions.contains(t.getAction())) {
+            mTransitions.add(t);
+        } else {
+            throw new InvalidTransitionException(t);
+        }
+    }
 
-	@Override
-	public void addState(STATE state) {
-		this.states.add(state);
-	}
+    @Override
+    public Set<A> getActions() {
+        return mActions;
+    }
 
-	@Override
-	public void addTransition(Transition<STATE, ACTION> t) throws FVMException {
-		boolean From = this.states.contains(t.getFrom());
-		boolean Action = this.actions.contains(t.getAction());
-		boolean To = this.states.contains(t.getTo());
+    @Override
+    public void addAtomicProposition(P p) {
+        if (!mAtomicPropositions.contains(p)) {
+            mAtomicPropositions.add(p);
+        }
+    }
 
-		if (!From || !To || !Action) {
-			throw new InvalidTransitionException(t);
-		}
-		this.transitions.add(t);
-	}
+    @Override
+    public Set<P> getAtomicPropositions() {
+        return mAtomicPropositions;
+    }
 
-	@Override
-	public Set<ACTION> getActions() {
-		return this.actions;
-	}
+    @Override
+    public void addToLabel(S s, P l) throws FVMException {
+        if (mStates.contains(s) && mAtomicPropositions.contains(l)) {
+            if (!mLabels.containsKey(s)) {
+                mLabels.put(s, new HashSet<P>());
+            }
 
-	@Override
-	public void addAtomicProposition(ATOMIC_PROPOSITION p) {
-		this.atomicPropositions.add(p);
-	}
+            if (!mLabels.get(s).contains(l)) {
+                mLabels.get(s).add(l);
+            }
 
-	@Override
-	public Set<ATOMIC_PROPOSITION> getAtomicPropositions() {
-		return this.atomicPropositions;
-	}
+        } else {
+            throw new InvalidLablingPairException(s, l);
+        }
+    }
 
-	@Override
-	public void addToLabel(STATE s, ATOMIC_PROPOSITION l) throws FVMException {
-		boolean ValidState = this.states.contains(s);
-		if (!ValidState) {
-			throw new InvalidLablingPairException(s, l);
-		} 
-		
-		boolean AtomicP = this.atomicPropositions.contains(l);
-		if (!AtomicP) {
-			throw new InvalidLablingPairException(s, l);
-		}
-		
-		Set<ATOMIC_PROPOSITION> StatesLables = this.labeled.containsKey(s) ? this.labeled.get(s) : new HashSet<ATOMIC_PROPOSITION>(); 
-		StatesLables.add(l);
-		this.labeled.put(s, StatesLables);
-	}
+    @Override
+    public Set<P> getLabel(S s) {
+        if (mStates.contains(s)) {
+            return mLabels.containsKey(s) ? mLabels.get(s) : new HashSet<>();
+        } else {
+            throw new StateNotFoundException(s);
+        }
+    }
 
-	@Override
-	public Set<ATOMIC_PROPOSITION> getLabel(Object s) {
-		if (!this.states.contains(s)) {
-			throw new StateNotFoundException(s);
-		}
+    @Override
+    public Set<S> getInitialStates() {
+        return mInitialStates;
+    }
 
-		return this.labeled.containsKey(s) ? this.labeled.get(s) : new HashSet<ATOMIC_PROPOSITION>();
-	}
+    @Override
+    public Map<S, Set<P>> getLabelingFunction() {
+        return mLabels;
+    }
 
-	@Override
-	public Set<STATE> getInitialStates() {
-		return this.initialStates;
-	}
+    @Override
+    public Set<S> getStates() {
+        return mStates;
+    }
 
-	@Override
-	public Map<STATE, Set<ATOMIC_PROPOSITION>> getLabelingFunction() {
-		return this.labeled;
-	}
+    @Override
+    public Set<Transition<S, A>> getTransitions() {
+        return mTransitions;
+    }
 
-	@Override
-	public Set<STATE> getStates() {
-		return this.states;
-	}
+    @Override
+    public void removeAction(A a) throws FVMException {
+        Iterator<Transition<S, A>> iterator = mTransitions.iterator();
+        while (iterator.hasNext()) {
+            Transition transition = iterator.next();
+            if (transition.getAction().equals(a)) {
+                throw new DeletionOfAttachedActionException(a, TransitionSystemPart.ACTIONS);
+            }
+        }
 
-	@Override
-	public Set<Transition<STATE,ACTION>> getTransitions() {
-		return this.transitions;
-	}
+        mActions.remove(a);
+    }
 
-	@Override
-	public void removeAction(ACTION action) throws FVMException {
-		for(Transition<STATE, ACTION> transition : this.transitions) {
-			// make sure the deleted action is not part of any transition
-			if (transition.getAction().equals(action)) {
-				throw new DeletionOfAttachedActionException(action, TRANSITIONS);
-			}
+    @Override
+    public void removeAtomicProposition(P p) throws FVMException {
+        for (Map.Entry<S, Set<P>> label : mLabels.entrySet()) {
+            if (label.getValue().contains(p)) {
+                throw new DeletionOfAttachedAtomicPropositionException(p, TransitionSystemPart.ATOMIC_PROPOSITIONS);
+            }
+        }
+        mAtomicPropositions.remove(p);
+    }
 
-			this.actions.remove(action);
-		}
-	}
+    @Override
+    public void removeInitialState(S s) {
+        mInitialStates.remove(s);
+    }
 
+    @Override
+    public void removeLabel(S s, P l) {
+        if (mLabels.containsKey(s)) {
+            mLabels.get(s).remove(l);
+            if (mLabels.get(s).size() == 0) {
+                mLabels.remove(s);
+            }
+        }
+    }
 
+    @Override
+    public void removeState(S s) throws FVMException {
+        Iterator<Transition<S, A>> iterator = mTransitions.iterator();
+        while (iterator.hasNext()) {
+            Transition transition = iterator.next();
+            if (transition.getFrom().equals(s) || transition.getTo().equals(s)) {
+                throw new DeletionOfAttachedStateException(s, TransitionSystemPart.STATES);
+            }
+        }
 
-	@Override
-	public void removeAtomicProposition(ATOMIC_PROPOSITION ap) throws FVMException {
-		for(Set<ATOMIC_PROPOSITION> labels : this.labeled.values()) {
-			// make sure the deleted AP is not used to label existing state
-			if (labels.contains(ap)) {
-				throw new DeletionOfAttachedAtomicPropositionException(ap, STATES);
-			}
-		}
+        if (!mLabels.containsKey(s) && !mInitialStates.contains(s)) {
+            mStates.remove(s);
+        } else {
+            throw new DeletionOfAttachedStateException(s, TransitionSystemPart.STATES);
+        }
+    }
 
-		this.atomicPropositions.remove(ap);
-	}
-
-
-	@Override
-	public void removeInitialState(STATE initialState) {
-		this.initialStates.remove(initialState);
-	}
-
-	@Override
-	public void removeLabel(STATE s, ATOMIC_PROPOSITION l) {
-		if (this.labeled.containsKey(s))
-		{
-			this.labeled.get(s).remove(l);
-		}
-	}
-	
-	@Override
-	public void removeState(STATE state) throws FVMException {
-		
-		//make sure the state does not take part in any transition
-		for(Transition<STATE, ACTION> transition : this.transitions) {
-			if (transition.getFrom().equals(state) || transition.getTo().equals(state)) {
-				throw new DeletionOfAttachedStateException(state, TRANSITIONS);
-			}
-		}
-		
-		//make sure the state is not labeled
-		Set<ATOMIC_PROPOSITION> labels = this.labeled.get(state);
-		if (labels != null && !labels.isEmpty()) {
-			throw new DeletionOfAttachedStateException(state, LABELING_FUNCTION);
-		}
-
-		// make the state is not an initial state 
-		if (this.initialStates.contains(state))
-		{
-			throw new DeletionOfAttachedStateException(state, INITIAL_STATES);
-		}
-		
-		this.states.remove(state);
-	}
-
-	@Override
-	public void removeTransition(Transition t) {
-		this.transitions.remove(t);
-	}
+    @Override
+    public void removeTransition(Transition<S, A> t) {
+        mTransitions.remove(t);
+    }
 }
